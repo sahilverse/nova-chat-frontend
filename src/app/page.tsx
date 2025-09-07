65f906585caff36b7ef2544afc46513f002cb0f1
Sahil Dahal <denenme49@gmail.com>
2025-09-07 11:45:19 +0545

    setup Shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 
 
 export default function Home() {
-  return <main>Welcome to Nova Chat App!</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
+    </main>
+  );
 }
